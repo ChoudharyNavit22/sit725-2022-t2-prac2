76d6a897a6fd48342ed81c96fcded36505705793
ChoudharyNavit22 <navitchoudhary22@gmail.com>
2022-07-27 17:56:03 +1000

    Updates - created package.json - created server.js file - initiated basic express server in the application

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+var port = process.env.port || 3000;
+
+app.listen(port,()=>{
+    console.log("App running at http://localhost:"+port)
+})
